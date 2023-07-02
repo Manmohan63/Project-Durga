f7dc01dba24ad523366311ea5b7e1f7202591c72
ishikapoddar <ishikapoddar30@gmail.com>
2023-07-02 12:03:17 +0530

    Made Community Form and made About  us responsive

--- project-durga/pages/about_us/index.jsx
+++ project-durga/pages/about_us/index.jsx
@@ -3,16 +3,21 @@ import Image from 'next/image'
 const Index = () => {
   return (
     <>
-    <div className="bg-cover bg-no-repeat clip-circle" style={{ backgroundImage: 'url("https://res.cloudinary.com/ddtzbznea/image/upload/v1687695328/women_ilfvsz.jpg")' }}>
-    <div className="flex flex-col justify-center items-center h-screen bg-gradient-to-r from-[#e1098356] to-[#e1098356] clip-circle">
+    <div>
+   
+    
+    <div className="flex justify-around items-center bg-origin-border min-w-screen min-h-screen bg-center bg-cover bg-no-repeat bg-[url('https://res.cloudinary.com/ddtzbznea/image/upload/v1687695328/women_ilfvsz.jpg')]">
+    <div className="flex flex-col h-screen w-screen justify-center items-center bg-gradient-to-r from-[#e1098356] to-[#e1098356] ">
       <h2 className="text-[white] text-6xl font-semibold mb-4">About Us</h2>
       <h4 className="text-[white] text-3xl mb-8">Helping create livelihood opportunities for<br />women across India, since 2011.</h4>
       {/* <a href="" className="bg-[white] text-[black] py-2 px-4 rounded-full hover:bg-gray-200">Learn More</a> */}
     </div>
   </div>
-  <div className='pl-20 pr-20'>
+  </div>
+  <div className=''>
+  <div className=' flex flex-col  px-20 items-center'>
     <div>
-  <h2 className='text-4xl'>Our History</h2><br/>
+  <h2 className='text-4xl pt-4'>Our History</h2><br/>
                 <p className='text-lg'>It was on the 25th of April 2011 that Srujna was founded. Jyotika Bhatia and Vaishali Shah started it
                     after talking to rescued human trafficking victims and realising the problems they had still been
                     facing out in the world. They found that being free from that environment did not put an end to
@@ -24,31 +29,32 @@ const Index = () => {
                     to women across India and acts as a bridge between these women and the external market so that they
                     can earn their independent income by themselves.</p>
   </div>
-  <div className='flex justify-evenly pt-6'>
+  <div className='flex sm:flex-wrap  justify-around  pt-6'>
     <div>
       <h1 className='text-4xl'>Our Mission</h1><br/>
-      <p className='text-lg w-1/2'>To facilitate work for women by creating a network of ground partners
+      <p className='text-lg '>To facilitate work for women by creating a network of ground partners
          and mobilizing resources for them.</p>
 
     </div>
     <div>
       <h1 className='text-4xl'>Our Vision</h1><br/>
-      <p className='text-lg w-1/2'>To be the largest not for profit platform enabling women impacted 
+      <p className='text-lg '>To be the largest not for profit platform enabling women impacted 
         by poverty to earn a respectable income across India.</p>
     </div>
-    <div className='w-1/3'>
+    <div className=''>
       <h1 className='text-4xl'>
       Our Values
       </h1><br/>
       <ul className='text-lg'>
-        <li>
-        Be Tansparent and Accountable</li>
-        <li>Be Impact First</li>
-        <li>Be Professional</li>
-        <li>Be Resourceful and Effective</li>
+        
+        Our values include being Tansparent and Accountable, being professional, resourceful and effective.
+        
+        
+        
         
       </ul>
     </div>
+  </div>
   </div>
   </div>
   <div className="bg-cover bg-no-repeat " style={{ backgroundImage: 'url("https://res.cloudinary.com/ddtzbznea/image/upload/v1687695328/women_ilfvsz.jpg")' }}>
@@ -58,7 +64,7 @@ const Index = () => {
         We are a young team, passionate about helping others and creating a better world. Srujna was started in
         2011, by Jyotika and Vaishali as a project and got registered in 2012 as a Charitable Trust.
       </p><br/>
-      <div className="trusteesInfoContener flex gap-6 pl-6 pr-6">
+      <div className="trusteesInfoContener flex sm:flex-wrap gap-6 pl-6 pr-6">
         <div className="trusteesInfo">
           <img src="https://res.cloudinary.com/ddtzbznea/image/upload/v1685636266/cld-sample.jpg" alt="imgError" className="w-full h-40" />
           <h4 className="text-2xl font-bold mt-4">Jyotika</h4><br/>
@@ -104,7 +110,7 @@ const Index = () => {
     <div className='pt-12'>
       <h1 className='text-4xl ml-8 pb-6 '>MEDIA COVERAGE</h1>
       <div className='flex flex-wrap justify-around gap-6'>
-        <div className=' overflow-hidden w-1/4 h-40'>
+        <div className=' overflow-hidden  w-1/4 h-40'>
       <img src="https://res.cloudinary.com/ddtzbznea/image/upload/v1685636266/cld-sample.jpg" alt="imgError" className="   custom-zoom" />
       </div>
       <div className=' overflow-hidden w-1/4 h-40'>
@@ -127,7 +133,7 @@ const Index = () => {
     </div>
     <div className='mt-24'>
     <section className="  bg-cover bg-no-repeat" style={{ backgroundImage: "url(https://res.cloudinary.com/ddtzbznea/image/upload/v1687695328/women_ilfvsz.jpg)" }}>
-      <div className=" pt-12 pb-32 p-5 flex justify-between bg-gradient-to-r from-[#000000d2] to-[#000000d2] text-[white]">
+      <div className=" pt-12 pb-32 flex sm:flex-wrap gap-6 pl-8 justify-around bg-gradient-to-r from-[#000000d2] to-[#000000d2] text-[white]">
         <div className=" flex flex-wrap gap-8 w-full ">
           
             <img src="https://res.cloudinary.com/ddtzbznea/image/upload/v1685636266/cld-sample.jpg" alt="" className='rounded-full w-1/3 h-1/3 ' />
@@ -148,12 +154,12 @@ const Index = () => {
             <img src="https://res.cloudinary.com/ddtzbznea/image/upload/v1685636266/cld-sample.jpg" alt="" className='rounded-full w-1/3 h-1/3' />
           
 
-          {/* Repeat the logo divs for Logo3 to Logo9 */}
+          
 
         </div>
         <div className=" flex justify-center items-center">
           <div className=" mr-16">
-            <button className=" mt-12 text-center text-lg p-4 rounded-md font-semibold tracking-wide  text-[black] bg-[#dd83a8] border-none">MORE THAN 56 COMPANIES</button>
+            <button className=" mt-12 text-center text-lg sm:p-2 p-4 rounded-md font-semibold tracking-wide  text-[black] bg-[#dd83a8] border-none">MORE THAN 56 COMPANIES</button>
             <h1 className=" text-4xl   mt-8 mb-4  ">Our Supporters</h1>
             <p className=" leading-7 pb-6">
               More than 56 companies and organizations have supported Srujna, financially and non-financially. We are grateful for their enthusiasm and commitment to the cause and the trust they have placed in us.
